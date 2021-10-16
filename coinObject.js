7b0dfc98eb7727058383cfee4dfd3751fb28fce7
Lucas <lcssreiser@gmail.com>
2021-10-16 18:06:32 -0300

    atividade pronta

---
+++ coinObject.js
@@ -0,0 +1,61 @@
+const coin = {
+    state: 0,
+  
+    flip: function () {
+        this.state = Math.floor(Math.random() * 2);
+    },
+  
+    toString: function () {
+        if(this.state === 0){
+            return "Heads"
+        }
+        else{
+            return "Tails"
+        }
+    },
+  
+    toHTML: function () {
+        const image = document.createElement("img");
+        if(this.state === 0){
+            image.src = "./coinObject/cara.png"
+            image.alt = "lado cara"
+        }
+        else{
+            image.src = "./coinObject/coroa.png"
+            image.alt = "lado coroa"
+        }
+        return image;
+    },
+
+};
+
+function display20Flips() {
+    const results = [];
+    const body = document.querySelector('body')
+    const text = document.createElement('h2')
+    body.appendChild(text)
+
+    for(let i = 0; i < 20; i++){
+        coin.flip()
+        results.push(coin.state)
+        text.innerHTML += `${coin.toString()} - `
+    }
+    return results
+
+}
+  
+function display20Images() {
+    const results = [];
+    const body = document.querySelector("body")
+    const imagens = document.createElement("div")
+    body.appendChild(imagens)
+    for(let i = 0; i < 20; i++) {
+        coin.flip()
+        results[i] = coin.state
+        imagens.appendChild(coin.toHTML())
+    }
+    return results
+}
+
+display20Flips()
+display20Images()
